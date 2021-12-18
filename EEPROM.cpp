9b90afdd3e3536d7c2a980104c5c494015dfc8e9
TheHiddenKnowledge <finney538@gmail.com>
2021-12-18 02:34:19 -0700

    Updated class methods to match previous changes

--- EEPROM.cpp
+++ EEPROM.cpp
@@ -8,53 +8,116 @@
 #include "EEPROM.h"
 
 // Constructors for the EEPROM class:
-EEPROM::EEPROM(int board, volatile byte* I1, volatile byte* O1, int ce, int we, int oe)
+EEPROM::EEPROM(int board, int CE, int WE, int OE)
 {
 	BOARD = board;
-	in1 = I1;
-	out1 = O1;
+	if (BOARD == 1)
+	{
+		a1 = &PORTB;
+		d1 = &PORTD;
+		da1 = a1 - 1;
+		dd1 = d1 - 1;
+	}
+	if (BOARD == 2)
+	{
+		a1 = &PORTA;
+		a2 = &PORTC;
+		d1 = &PORTK;
+		da1 = a1 - 1;
+		da2 = a2 - 1;
+		dd1 = d1 - 1;
+	}
+	else
+	{
+		return
+	}
+	ce = CE;
+	we = WE;
+	oe = OE;
 }
 
-EEPROM::EEPROM(int board, volatile byte* I1, volatile byte* I2, volatile byte* O1, volatile byte* O2, int ce, int we, int oe)
+EEPROM::EEPROM(int board, volatile byte *A1, volatile byte *D1, int CE, int WE, int OE)
 {
 	BOARD = board;
-	in1 = I1;
-	in2 = I2;
-	out1 = O1;
-	out2 = O2;
-}
-
-void EEPROM::writeEEP(unsigned char data, unsigned short address) {
-	if (BOARD == 1) {
-
+	if (BOARD == 1)
+	{
+		a1 = A1;
+		d1 = D1;
+		da1 = a1 - 1;
+		dd1 = d1 - 1;
+		ce = CE;
+		we = WE;
+		oe = OE;
 	}
-	if (BOARD == 2) {
-		DDRA |= 0xFF;
-		DDRC |= 0xFF;
-		DDRK |= 0xFF;
-		DDRG |= 0x03;
-		PORTA |= (address & 0x00FF);
-		PORTC |= (address >> 8);
-		PORTG |= 0x01;
-		PORTK |= data;
+	else
+	{
+		return;
 	}
 }
-unsigned char EEPROM::readEEP(unsigned short address) {
-	if (BOARD == 1) {
-		DDRD |= 0xFF;
-		DDRB |= 0xF0;
-		PORTD |= (address | 0x0F);
-		PORTB |= (0x2<<4);
-		return (PINB|0x0F);
+
+EEPROM::EEPROM(int board, volatile byte *A1, volatile byte *A2, volatile byte *D1, int CE, int WE, int OE)
+{
+	BOARD = board;
+	if (BOARD == 1)
+	{
+		a1 = A1;
+		a2 = A2;
+		d1 = D1;
+		da1 = a1 - 1;
+		da2 = a2 - 1;
+		dd1 = d1 - 1;
+		ce = CE;
+		we = WE;
+		oe = OE;
 	}
-	if (BOARD == 2) {
-		DDRA |= 0xFF;
-		DDRC |= 0xFF;
-		DDRK |= 0x00;
-		DDRG |= 0X03;
-		PORTA |= (address | 0x0F);
-		PORTD |= (address >> 8);
-		PORTG |= 0x02;
-		return PINK;
-	}	
+	else
+	{
+		return;
+	}
+}
+
+void EEPROM::writeEEP(unsigned char data, unsigned short address)
+{
+	digitalWrite(ce, LOW);
+	digitalWrite(oe, HIGH);
+	digitalWrite(we, LOW);
+	if (BOARD == 1)
+	{
+		*da1 |= 0xFF;
+		*dd1 |= 0xFF;
+		*a1 |= (address & 0x000F);
+		*d1 |= data;
+	}
+	if (BOARD == 2)
+	{
+		*da1 |= 0xFF;
+		*da2 |= 0xFF;
+		*dd1 |= 0xFF;
+		*a1 |= (address & 0x00FF);
+		*a2 |= (address >> 8);
+		*d1 |= data;
+	}
+	digitalWrite(we, HIGH);
+	digitalWrite(oe, HIGH);
+	digitalWrite(ce, HIGH);
+}
+
+byte EEPROM::readEEP(unsigned short address)
+{
+	if (BOARD == 1)
+	{
+		*da1 |= 0xFF;
+		*dd1 |= 0x00;
+		*a1 |= (address & 0x000F);
+		return *d1;
+	}
+	if (BOARD == 2)
+	{
+		*da1 |= 0xFF;
+		*da2 |= 0xFF;
+		*dd1 |= 0x00;
+		*a1 |= (address & 0x00FF);
+		*a2 |= (address >> 8);
+		return *d1;
+	}
 }
