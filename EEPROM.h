22cec69357e1abb0f6654d2a2e01100d1156f0ae
TheHiddenKnowledge <finney538@gmail.com>
2021-12-18 02:35:02 -0700

    Added fields for port direction pointers

--- EEPROM.h
+++ EEPROM.h
@@ -1,16 +1,26 @@
 #ifndef EEPROM_h
 #define EEPROM_h
 
+#include <Arduino.h>
+#include <io.h>
+
 class EEPROM {
 	public:
-		EEPROM(int board, volatile byte* I1, volatile byte* O1, int CE, int WE, int OE);
-		EEPROM(int board, volatile byte* I1, volatile byte* I2, volatile byte* O1, volatile byte* O2, int CE, int WE, int OE);
-		volatile byte* in1;
-		volatile byte* in2; 
-		volatile byte* out1;
-		volatile byte* out2; 
-		int 
+		EEPROM(int board, int CE, int WE, int OE);
+		EEPROM(int board, volatile byte* A1, volatile byte* D1, int CE, int WE, int OE);
+		EEPROM(int board, volatile byte* A1, volatile byte* A2, volatile byte* D1, int CE, int WE, int OE);
+		volatile byte* a1;
+		volatile byte* a2; 
+		volatile byte* d1;
+		volatile byte* da1;
+		volatile byte* da2; 
+		volatile byte* dd1;
+		int ce;
+		int we;
+		int oe; 
 		int BOARD;
 		void writeEEP(unsigned char data, unsigned short address);
 		void readEEP(unsigned short address);
 };
+
+#endif
