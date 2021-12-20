b684e029c20527a794d2d3e0bcc2401e3c03ab43
TheHiddenKnowledge <finney538@gmail.com>
2021-12-20 14:07:13 -0700

    Changed code to match the previous revisions

--- examples/EEPROM_Programmer/EEPROM_Programmer.ino
+++ examples/EEPROM_Programmer/EEPROM_Programmer.ino
@@ -1,8 +1,8 @@
-//A0-A3 is PD0-PD3, D0-D3 is PD4-PD7, OE is PB0, CE/WE is PB1  
 #include <EEP.h>
-EEP EEP(2); 
+
+EEP EEP(39, 41, 40); 
+
 void setup() {
-  // put your setup code here, to run once:
    Serial.begin(9600);
    EEP.writeEEP(0xFC,0xCCCC);
    Serial.println("Byte Read: ");
