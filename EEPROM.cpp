e818ec79a1756d150d7a331793a3681607ad1146
TheHiddenKnowledge <finney538@gmail.com>
2021-12-20 13:41:37 -0700

    Matched the current code with the revisions made

--- EEPROM.cpp
+++ EEPROM.cpp
@@ -1,31 +1,23 @@
-/* 
-	Created by Isaiah Finney 
-	Feel free to use 
-	This is a library designed to read, write, and manage the memory of Microchip's AT28C256-15PU 32 KB IC package.
-	The chip can interface with the arduino Mega and Uno, but interfacing with the Uno will results in loss of storage capabilites.
-*/
-
 #include "EEPROM.h"
 
-// Constructors for the EEPROM class:
-EEPROM::EEPROM(int board, int CE, int WE, int OE)
+EEPROM::EEPROM(int CE, int WE, int OE)
 {
-	BOARD = board;
+	// These default ports are what I have preferred using for my board setup and I recommend if you don't need to use the other pins.
 	if (BOARD == 1)
 	{
 		a1 = &PORTB;
+		a2 = &PORTC;
 		d1 = &PORTD;
-		da1 = a1 - 1;
-		dd1 = d1 - 1;
+		*(a1 - 1) |= 0x3F;
+		*(a2 - 1) |= 0x3F;
 	}
-	if (BOARD == 2)
+	else if (BOARD == 2)
 	{
 		a1 = &PORTA;
 		a2 = &PORTC;
 		d1 = &PORTK;
-		da1 = a1 - 1;
-		da2 = a2 - 1;
-		dd1 = d1 - 1;
+		*(a1 - 1) |= 0xFF;
+		*(a2 - 1) |= 0xFF;
 	}
 	else
 	{
@@ -34,90 +26,161 @@ EEPROM::EEPROM(int board, int CE, int WE, int OE)
 	ce = CE;
 	we = WE;
 	oe = OE;
+	digitalWrite(ce, HIGH);
+	digitalWrite(oe, HIGH);
+	digitalWrite(we, HIGH);
 }
 
-EEPROM::EEPROM(int board, volatile byte *A1, volatile byte *D1, int CE, int WE, int OE)
+EEPROM::EEPROM(volatile uint8_t *A1, volatile uint8_t *A2, volatile uint8_t *D1, int CE, int WE, int OE)
 {
 	BOARD = board;
+	a1 = A1;
+	a2 = A2;
+	d1 = D1;
+	// For the UNO, there are only 3 ports, with one having a fullset of eight pins. The other two ports have 6 pins each 
+	// and are used for the memory address, giving a total of 12 bits for the address. 
 	if (BOARD == 1)
 	{
-		a1 = A1;
-		d1 = D1;
-		da1 = a1 - 1;
-		dd1 = d1 - 1;
-		ce = CE;
-		we = WE;
-		oe = OE;
+		*(a1 - 1) |= 0x3F;
+		*(a2 - 1) |= 0x3F;
+	}
+	// The Mega has many pins/ports to offer and can provide all 15 bits for the memory address.
+	else if (BOARD == 2)
+	{
+		*(a1 - 1) |= 0xFF;
+		*(a2 - 1) |= 0xFF;
 	}
 	else
 	{
 		return;
 	}
+	ce = CE;
+	we = WE;
+	oe = OE;
+	digitalWrite(ce, HIGH);
+	digitalWrite(oe, HIGH);
+	digitalWrite(we, HIGH);
 }
 
-EEPROM::EEPROM(int board, volatile byte *A1, volatile byte *A2, volatile byte *D1, int CE, int WE, int OE)
-{
-	BOARD = board;
-	if (BOARD == 1)
-	{
-		a1 = A1;
-		a2 = A2;
-		d1 = D1;
-		da1 = a1 - 1;
-		da2 = a2 - 1;
-		dd1 = d1 - 1;
-		ce = CE;
-		we = WE;
-		oe = OE;
-	}
-	else
-	{
-		return;
-	}
-}
-
-void EEPROM::writeEEP(unsigned char data, unsigned short address)
+void EEPROM::writeWord(uint8_t data, uint16_t address)
 {
 	digitalWrite(ce, LOW);
-	digitalWrite(oe, HIGH);
 	digitalWrite(we, LOW);
 	if (BOARD == 1)
 	{
-		*da1 |= 0xFF;
-		*dd1 |= 0xFF;
-		*a1 |= (address & 0x000F);
-		*d1 |= data;
+		if (address < MAXA1 && data < MAXD)
+		{
+			*(d1 - 1) |= 0xFF;
+			*a1 |= (address & 0x003F);
+			*a2 |= adress >> 6;
+			*d1 |= data;
+		}
+		else
+		{
+			return
+		}
 	}
 	if (BOARD == 2)
 	{
-		*da1 |= 0xFF;
-		*da2 |= 0xFF;
-		*dd1 |= 0xFF;
-		*a1 |= (address & 0x00FF);
-		*a2 |= (address >> 8);
-		*d1 |= data;
+		if (address < MAXA2 && data < MAXD)
+		{
+			*(d1 - 1) |= 0xFF;
+			*a1 |= (address & 0x00FF);
+			*a2 |= (address >> 8);
+			*d1 |= data;
+		}
+		else
+		{
+			return
+		}
 	}
 	digitalWrite(we, HIGH);
-	digitalWrite(oe, HIGH);
 	digitalWrite(ce, HIGH);
 }
 
-byte EEPROM::readEEP(unsigned short address)
+void EEPROM::writePage(uint8_t *data, uint8_t datasize, uint16_t start)
 {
 	if (BOARD == 1)
 	{
-		*da1 |= 0xFF;
-		*dd1 |= 0x00;
-		*a1 |= (address & 0x000F);
-		return *d1;
+		if (start + datasize > MAXA1 || datasize > 64)
+		{
+			return;
+		}
 	}
 	if (BOARD == 2)
 	{
-		*da1 |= 0xFF;
-		*da2 |= 0xFF;
-		*dd1 |= 0x00;
-		*a1 |= (address & 0x00FF);
-		*a2 |= (address >> 8);
-		return *d1;
+		if (start + datasize > MAXA2 || datasize > 64)
+		{
+			return;
+		}
 	}
+	*(d1 - 1) |= 0xFF;
+	for (int i = 0; i < datasize; i++)
+	{
+		digitalWrite(ce, LOW);
+		digitalWrite(we, LOW);
+		if (BOARD == 1)
+		{
+			if (data < MAXD)
+			{
+				*a1 |= (address & 0x003F);
+				*a2 |= adress >> 6;
+				*d1 |= data;
+			}
+			else
+			{
+				return
+			}
+		}
+		if (BOARD == 2)
+		{
+			if (data < MAXD)
+			{
+				*a1 |= (address & 0x00FF);
+				*a2 |= (address >> 8);
+				*d1 |= data;
+			}
+			else
+			{
+				return
+			}
+		}
+		digitalWrite(ce, HIGH);
+		digitalWrite(we, HIGH);
+	}
+}
+
+uint8_t EEPROM::readWord(uint16_t address)
+{
+	digitalWrite(ce, LOW);
+	digitalWrite(oe, LOW);
+	*(d1 - 1) |= 0x00;
+	if (BOARD == 1)
+	{
+		if (address < MAXA1 && data < MAXD)
+		{
+			*a1 |= (address & 0x003F);
+			*a2 |= adress >> 6;
+			return *d1;
+		}
+		else
+		{
+			return;
+		}
+	}
+	if (BOARD == 2)
+	{
+		if (address < MAXA2 && data < MAXD)
+		{
+			*a1 |= (address & 0x00FF);
+			*a2 |= (address >> 8);
+			return *d1;
+		}
+		else
+		{
+			return;
+		}
+	}
+	digitalWrite(oe, HIGH);
+	digitalWrite(ce, HIGH);
 }
